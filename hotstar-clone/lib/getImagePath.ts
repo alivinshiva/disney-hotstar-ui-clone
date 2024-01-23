39c3d3bbbbf251254c4a3c81b413d824a008a336
Alivin Shiva <alivin612003@gmail.com>
2024-01-24 02:16:33 +0530

    removed cached file

--- hotstar-clone/lib/getImagePath.ts
+++
@@ -1,6 +0,0 @@
-const getImagePath = (imagePath?: string, fullsize?: boolean)=>{
-    return imagePath
-        ? `http://image.tmdb.org/t/p/${fullsize ? "original" : "w500"}/${imagePath}`
-        : "https://links.papareact.com/o8z"
-}
-export default getImagePath;
